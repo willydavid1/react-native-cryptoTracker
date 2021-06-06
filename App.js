a08edb90bc27056ba97ecf61acb7d72a9ee45d44
Willy Da Conceicao <willydavidlozada@gmail.com>
2021-06-05 20:36:52 -0500

    Adding my first tab

--- App.js
+++ App.js
@@ -1,11 +1,35 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native'
 import CoinsStack from './src/components/coins/CoinsStack.js'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { Image } from 'react-native'
+import Colors from './src/res/colors'
+
+const Tabs = createBottomTabNavigator()
 
 const App = () => {
   return (
     <NavigationContainer>
-      <CoinsStack />
+      <Tabs.Navigator
+        tabBarOptions={{
+          style: {
+            backgroundColor: Colors.blackPearl
+          }
+        }}
+      >
+        <Tabs.Screen
+          name="Coins"
+          component={CoinsStack}
+          options={{
+            tabBarIcon: ({ size, color }) => (
+              <Image
+                style={{ tintColor: color, width: size, height: size }}
+                source={require('./src/assets/bank.png')}
+              />
+            )
+          }}
+        />
+      </Tabs.Navigator>
     </NavigationContainer>
   )
 };
